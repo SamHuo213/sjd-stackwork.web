adc356cb31ced3c7690a542e17d198132d2c6982
Sam Huo <hhuo213@hotmail.com>
2026-02-03 02:26:14 +0700

    More changes

--- components/layout/container.tsx
+++ components/layout/container.tsx
@@ -1,16 +1,19 @@
 import { cn } from "@/lib/utils";
 
 interface ContainerProps extends React.HTMLAttributes<HTMLDivElement> {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 export function Container({ children, className, ...props }: ContainerProps) {
-    return (
-        <div
-            className={cn("mx-auto w-full max-w-7xl px-4 md:px-6 lg:px-10", className)}
-            {...props}
-        >
-            {children}
-        </div>
-    );
+  return (
+    <div
+      className={cn(
+        "mx-auto w-full max-w-7xl px-4 md:px-6 lg:px-12",
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </div>
+  );
 }
